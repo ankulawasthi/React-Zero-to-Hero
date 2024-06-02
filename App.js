c543a605ac3f9763d84e84561201cceca0ed5dc8
Ankul Awasthi <awasthi.ankul121@gmail.com>
2024-06-02 16:00:24 +0530

    Day 2

---
+++ App.js
@@ -0,0 +1,12 @@
+const parent = React.createElement(
+    "div",
+    {id: "parent"},
+    React.createElement(
+        "div",
+        {id:"child"},
+        React.createElement("h1", {}, "This is my h1 heading")
+    )
+);
+console.log(parent);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
